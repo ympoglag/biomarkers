5c78fb14a7fac7f323b7e5dbb0700b167da755ff
Ympoglag <ympoglag@gmail.com>
2025-07-08 18:57:38 +0200

    WIP

---
+++ template.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# tsv_to_html() {
+#     awk 'BEGIN { print "<table border=1>" }
+#         { print "<tr>"; for(i=1;i<=NF;i++) print "<td>" $i "</td>"; print "</tr>" }
+#     END { print "</table>" }' FS='\t' "./biomarkers.tsv" >"./biomarkers.html"
+# }
+
+tsv_to_html() {
+	awk 'BEGIN { print "<table border=1>" }
+        NR==1 {
+            print "<tr>"
+            for(i=1; i<=NF; i++) print "<th>" $i "</th>"
+            print "</tr>"
+            next
+        }
+        {
+            print "<tr>"
+            for(i=1; i<=NF; i++) print "<td>" $i "</td>"
+            print "</tr>"
+        }
+    END { print "</table>" }' FS='\t' "./biomarkers.tsv" >"./biomarkers.html"
+}
+
+template() {
+	cp "$1" "index.html"
+	grep -o '<!-- INCLUDE [^ ]\+ -->' "$1" | while read -r inc; do
+		file=$(echo "$inc" | sed -E 's/<!-- INCLUDE ([^ ]+) -->/\1/')
+		sed -i "/$inc/{
+        r $file
+        d
+      }" "index.html"
+	done
+}
+
+autoformat_html() {
+	tmpfile=$(mktemp)
+	cleanup_tmpfile() {
+		rm -f "$tmpfile"
+	}
+	trap 'cleanup_tmpfile' EXIT INT
+
+	args=(
+		--custom-tags yes
+		--indent yes
+		--wrap-attributes 1
+		-w 140
+		--quiet 1
+		--tidy-mark 0
+		--break-before-br
+	)
+
+	if tidy "${args[@]}" "$1" >"$tmpfile"; then
+		mv "$tmpfile" "$1"
+	else
+		echo "tidy failed: $1"
+		rm "$tmpfile"
+		exit 1
+	fi
+}
+
+tsv_to_html
+template "./index.template.html"
+autoformat_html "./index.html"
