316123fbab28ea96257919075a2b28fdff0f2147
Ympoglag <ympoglag@gmail.com>
2025-07-10 11:07:53 +0200

    WIP 1752138473

--- template.sh
+++ template.sh
@@ -8,7 +8,7 @@ set -eo pipefail
 # }
 
 tsv_to_html() {
-	awk 'BEGIN { print "<table border=1>" }
+    awk 'BEGIN { print "<table border=1>" }
         NR==1 {
             print "<tr>"
             for(i=1; i<=NF; i++) print "<th>" $i "</th>"
@@ -24,43 +24,42 @@ tsv_to_html() {
 }
 
 template() {
-	cp "$1" "index.html"
-	grep -o '<INCLUDE [^ ]\+/>' "$1" | while read -r inc; do
-		file=$(echo "$inc" | sed -E 's#<INCLUDE ([^ ]+)/>.*#\1#')
-		# Escape sed special characters in inc for the search
-		pat=$(printf '%s\n' "$inc" | sed 's/[][\.*^$/]/\\&/g')
-		sed -i "/$pat/{
+    cp "$1" "index.html"
+    grep -o '<INCLUDE [^ ]\+/>' "$1" | while read -r inc; do
+        file=$(echo "$inc" | sed -E 's#<INCLUDE ([^ ]+)/>.*#\1#')
+        # Escape sed special characters in inc for the search
+        pat=$(printf '%s\n' "$inc" | sed 's/[][\.*^$/]/\\&/g')
+        sed -i "/$pat/{
             r $file
             d
         }" "index.html"
-	done
+    done
 }
 
 autoformat_html() {
-	tmpfile=$(mktemp)
-	cleanup_tmpfile() {
-		rm -f "$tmpfile"
-	}
-	trap 'cleanup_tmpfile' EXIT INT
+    tmpfile=$(mktemp)
+    cleanup_tmpfile() {
+        rm -f "$tmpfile"
+    }
+    trap 'cleanup_tmpfile' EXIT INT
 
-	args=(
-		--custom-tags yes
-		--indent yes
-		--wrap-attributes 1
-		-w 140
-		--quiet 1
-		--tidy-mark 0
-		--break-before-br
+    args=(
+        --custom-tags yes
+        --indent yes
+        --wrap-attributes 1
+        -w 140
+        --quiet 1
+        --tidy-mark 0
+        --break-before-br
         --drop-empty-elements no
-	)
-
-	if tidy "${args[@]}" "$1" >"$tmpfile"; then
-		mv "$tmpfile" "$1"
-	else
-		echo "tidy failed: $1"
-		rm "$tmpfile"
-		exit 1
-	fi
+    )
+    if tidy "${args[@]}" "$1" >"$tmpfile"; then
+        mv "$tmpfile" "$1"
+    else
+        echo "tidy failed: $1"
+        rm "$tmpfile"
+        exit 1
+    fi
 }
 
 tsv_to_html
